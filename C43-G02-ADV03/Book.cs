90089d04667f1c8af08b81c3d7898b8cfee89c10
youssef_74 <youssefessam1293@gmail.com>
2025-02-06 15:15:50 +0200

    Delegate

---
+++ C43-G02-ADV03/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G02_ADV03
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string iSBN, string title, string[] authors, DateTime publicationDate, decimal price)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Authors = authors;
+            PublicationDate = publicationDate;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Title: {Title}, ISBN: {ISBN}, Authors: {string.Join(", ", Authors)}, Date: {PublicationDate.ToShortDateString()}, Price: {Price:C}";
+        }
+
+    }
+}
